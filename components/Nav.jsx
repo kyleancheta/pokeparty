1f8c01afd83308b29fb0b8a224aa7cb1eb70f3b4
Kyle Ancheta <kyleancheta@live.com>
2023-07-01 19:25:38 -0700

    Not sure if I fixed the ball but I hope so...

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Nav() {
     return (
         <nav className="navigation">
-            <img src="https://github.com/kyleancheta/pokeparty/blob/main/img/pokeball.png" className="pokeball"/>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png" className="pokeball"/>
             <h3 className="site-title">pokeparty.fun</h3>
         </nav>
     )
