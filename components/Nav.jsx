9190d70947373e9802ae2f87d50259c058b91bdf
Kyle Ancheta <kyleancheta@live.com>
2023-07-03 01:03:36 -0700

    css module for the nav, global colors and yuh

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import navStyles from "./Nav.module.css"
 
 export default function Nav() {
     return (
-        <nav className="navigation">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png" className="pokeball"/>
+        <nav className={navStyles.navigation}>
+            <img className={navStyles.pokeball} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png"/>
             <h3 className="site-title">pokeparty.fun</h3>
         </nav>
     )
