411169dbf5fa86c0b96daeb753da83b73ee6b003
Kyle Ancheta <kyleancheta@live.com>
2023-07-10 23:41:14 -0700

    Introduced icons and fixed some colors.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import navStyles from "./Nav.module.css"
+import { MdInfoOutline } from "react-icons/md";
 
 export default function Nav(props) {
     return (
         <nav className={navStyles.navigation}>
             <img className={navStyles.pokeball} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png"/>
             <h3 className="site-title">pokeparty.fun</h3>
-            <button className={navStyles.infoBtn} onClick={props.toggleInfo}>Huh?</button>
+            <button className={navStyles.infoBtn} onClick={props.toggleInfo}>
+                <MdInfoOutline className={navStyles.icon}/>
+            </button>
         </nav>
     )
 }
