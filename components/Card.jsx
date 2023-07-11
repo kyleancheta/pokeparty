0e67a1fd42dc1ce090f602d0839faeb68b6c3aa5
Kyle Ancheta <kyleancheta@live.com>
2023-07-10 20:32:35 -0700

    Refactored the cards component.

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,15 +1,16 @@
 import React from "react"
+import cardStyles from "./Card.module.css"
 
 export default function Card(props) {
     return (
-        <div className="card">
-            <div className="container-images">
-                <img src={props.image} className="pokemon-artwork-main"/>
+        <div className={cardStyles.card}>
+            <div className={cardStyles.container}>
+                <img src={props.image} className={cardStyles.artwork}/>
             </div>
-            <h2 className="pokemon-name">{props.name}</h2>
-            <section className="pokemon-types">
-                <div className={`pill-type ${props.typeOne}`}>{props.typeOne}</div>
-                {props.typeTwo && <div className={`pill-type ${props.typeTwo}`}>{props.typeTwo}</div>}
+            <h2 className={cardStyles.name}>{props.name}</h2>
+            <section className={cardStyles.types}>
+                <div className={`${cardStyles.pill} ${props.typeOne}`}>{props.typeOne}</div>
+                {props.typeTwo && <div className={`${cardStyles.pill} ${props.typeTwo}`}>{props.typeTwo}</div>}
             </section>
         </div>
     )
