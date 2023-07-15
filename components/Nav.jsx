d8eb1de486c9bf8e48b03739e53a2c24ee37c93c
Kyle Ancheta <kyleancheta@live.com>
2023-07-15 01:21:14 -0700

    quick nav fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,7 +10,7 @@ export default function Nav({randomizeParty, toggleInfo, ...props}) {
                 <MdSync className={navStyles.icon}/>
             </Button>
             
-            <section class={navStyles.logo}>
+            <section className={navStyles.logo}>
                 <img className={navStyles.pokeball} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png"/>
                 <h3 className="site-title">pokeparty.fun</h3>
             </section>
