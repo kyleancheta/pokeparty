3244e3352d2af33e9b8ca5779b75f05752515e0b
Kyle Ancheta <kyleancheta@live.com>
2023-08-03 01:05:25 -0700

    portfolio

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
     return (
         <div className="footer">
-            Made for fun, by <a href="https://google.com" target="_blank" rel="noopener noreferrer"> Kyle</a>.
+            Made for fun, by <a href="https://www.kyleancheta.me/" target="_blank" rel="noopener noreferrer"> Kyle</a>.
         </div>
     )
 }
