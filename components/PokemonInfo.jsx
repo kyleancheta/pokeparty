aa426064235c9eed5e73081337e06dced1e78567
Kyle Ancheta <kyleancheta@live.com>
2023-08-04 01:48:26 -0700

    peak modes

--- components/PokemonInfo.jsx
+++ components/PokemonInfo.jsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { cloneElement } from "react"
 import pokeStyles from "./PokemonInfo.module.css"
 import Button from "./Button"
 import Stat from "./Stat"
-import { MdClose, MdArrowBack, MdArrowForward } from "react-icons/md";
+import { MdClose, MdArrowBack, MdArrowForward, MdKeyboardTab, MdWidthNormal } from "react-icons/md";
 
 function PokemonInfo({pokemon, activeIndex, close, prevPoke, nextPoke, ...props}) {
+    const [centerPeak, setCenterPeak] = React.useState(false)
+
     const activePokemon = pokemon[activeIndex]
     const pokeHeight = activePokemon.height / 10
     const pokeWeight = activePokemon.weight / 10
@@ -14,6 +16,34 @@ function PokemonInfo({pokemon, activeIndex, close, prevPoke, nextPoke, ...props}
     if (activePokemon.types.length > 1) {
         typeTwo = activePokemon.types[1].type.name
     }
+
+    function toggleCenterPeak() {
+        setCenterPeak(state => !state)
+    }
+
+    // function handleKeyPress(event) {
+    //     console.log("current poke: " + activeIndex)
+
+    //     if (event.key === 'n' || event.keyCode === 39) {
+    //         nextPoke()
+    //     }
+    //     else if (event.key === 'p' || event.keyCode === 37) {
+    //         prevPoke()
+    //     }
+    //     else if (event.key === 'Escape') {
+    //         close()
+    //     }
+    //     else {
+    //         console.log("Not a valid control key")
+    //     }
+    // }
+
+    // React.useEffect(() => {
+    //     window.addEventListener("keyup", handleKeyPress)
+    //     return () => {
+    //         window.removeEventListener("keyup", handleKeyPress);
+    //     }
+    // }, [])
 
     const pokeStats = {
         "hp": activePokemon.stats[0].base_stat,
@@ -25,11 +55,17 @@ function PokemonInfo({pokemon, activeIndex, close, prevPoke, nextPoke, ...props}
     }
     const bst = activePokemon.stats[0].base_stat + activePokemon.stats[1].base_stat + activePokemon.stats[2].base_stat + activePokemon.stats[3].base_stat + activePokemon.stats[4].base_stat + activePokemon.stats[5].base_stat
 
-    // console.log(activePokemon)
     return (
         <div className={pokeStyles.bg}>
-            <section className={pokeStyles.main}>
+            <section className={ centerPeak ? pokeStyles.mainCenter : pokeStyles.main}>
                 <section className={pokeStyles.actions}>
+                    <Button onClick={toggleCenterPeak} className={pokeStyles.desktopDisplayOnly}>
+                        {
+                            centerPeak 
+                            ?   <MdKeyboardTab className={pokeStyles.icon}/>
+                            :   <MdWidthNormal className={pokeStyles.icon}/>
+                        }
+                    </Button>
                     <section className={pokeStyles.navActions}>    
                         <Button onClick={prevPoke} tooltip="Previous Pokemon" tooltipPosition="bottom">
                             <MdArrowBack className={pokeStyles.icon}/>
@@ -39,42 +75,52 @@ function PokemonInfo({pokemon, activeIndex, close, prevPoke, nextPoke, ...props}
                             <MdArrowForward className={pokeStyles.icon}/>
                         </Button>
                     </section>
-                        <Button onClick={close}>
-                            <MdClose className={pokeStyles.icon}/>
-                        </Button>
+                    <Button onClick={close}>
+                        <MdClose className={pokeStyles.icon}/>
+                    </Button>
                 </section>
-                <section className={pokeStyles.image}>
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${activePokemon.id}.png`} alt={activePokemon.name} />
+               
+                <section className={`${pokeStyles.section} ${pokeStyles.contentA}`}>
+                    <section className={pokeStyles.image}>
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${activePokemon.id}.png`} alt={activePokemon.name} />
+                    </section>
+                    <section className={pokeStyles.name}>
+                        <h1>{activePokemon.species.name}</h1>
+                        <h4>#{activePokemon.id}</h4>
+                    </section>
+                    <section className={pokeStyles.types}>
+                        <h4>Types</h4>
+                        <section className={pokeStyles.group}>
+                            <div className={`${pokeStyles.pill} ${typeOne}`}><p>{typeOne}</p></div>
+                            {typeTwo && <div className={`${pokeStyles.pill} ${typeTwo}`}><p>{typeTwo}</p></div>}
+                        </section>
+                    </section>
+                    {
+                        centerPeak 
+                        ?   null
+                        :   <hr className={pokeStyles.line}/>
+                    }
+                    {/* <hr className={pokeStyles.line}/> */}
                 </section>
-                <section className={pokeStyles.name}>
-                    <h1>{activePokemon.species.name}</h1>
-                    <h4>#{activePokemon.id}</h4>
-                </section>
-                <section className={pokeStyles.types}>
-                    <h4>Types</h4>
-                    <section className={pokeStyles.group}>
-                        <div className={`${pokeStyles.pill} ${typeOne}`}><p>{typeOne}</p></div>
-                        {typeTwo && <div className={`${pokeStyles.pill} ${typeTwo}`}><p>{typeTwo}</p></div>}
+                <section className={`${pokeStyles.section} ${pokeStyles.contentB}`}>
+                    <section className={pokeStyles.info}>
+                        <Stat label="Height" stat={`${pokeHeight}m`}/>
+                        <Stat label="Weight" stat={`${pokeWeight}kg`}/>
+                    </section>
+                    <hr className={pokeStyles.line}/>
+                    <section className={pokeStyles.stats}>
+                        <Stat label="HP" stat={pokeStats.hp}/>
+                        <Stat label="Attack" stat={pokeStats.attack}/>
+                        <Stat label="Defense" stat={pokeStats.defense}/>
+                        <Stat label="Special Attack" stat={pokeStats.spAttack}/>
+                        <Stat label="Special Defense" stat={pokeStats.spDefense}/>
+                        <Stat label="Speed" stat={pokeStats.speed}/>
+                    </section>
+                    <section className={pokeStyles.bst}>
+                        <Stat center label="Base Stat Total" stat={bst}/>
                     </section>
                 </section>
-                <hr className={pokeStyles.line}/>
-                <section className={pokeStyles.info}>
-                    <Stat label="Height" stat={`${pokeHeight}m`}/>
-                    <Stat label="Weight" stat={`${pokeWeight}kg`}/>
-                </section>
-                <hr className={pokeStyles.line}/>
-                <section className={pokeStyles.stats}>
-                    <Stat label="HP" stat={pokeStats.hp}/>
-                    <Stat label="Attack" stat={pokeStats.attack}/>
-                    <Stat label="Defense" stat={pokeStats.defense}/>
-                    <Stat label="Special Attack" stat={pokeStats.spAttack}/>
-                    <Stat label="Special Defense" stat={pokeStats.spDefense}/>
-                    <Stat label="Speed" stat={pokeStats.speed}/>
-                </section>
-                <section className={pokeStyles.bst}>
-                    <Stat center label="Base Stat Total" stat={bst}/>
-                </section>
-                <br/><br/>
+                {/* <br/><br/> */}
                 
             </section>
         </div>
